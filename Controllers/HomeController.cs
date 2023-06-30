3bc64d2d96fc376fb78ece5e3f7fa078bf692692
chayma <hedhli.chaimae@gmail.com>
2023-06-30 18:50:41 +0200

    CatMash-Liste des chats avec leur score

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,55 @@
-﻿using CatMash.Models;
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CatMash.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CatMash.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
-        public IActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            string url = "https://latelier.co/data/cats.json";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonData = await response.Content.ReadAsStringAsync();
+                    JObject jsonObject = JObject.Parse(jsonData);
+                    JArray imagesArray = jsonObject["images"] as JArray;
+
+                    List<Cat> cats = new List<Cat>();
+                    foreach (JToken imageToken in imagesArray)
+                    {
+                        string id = imageToken["id"].ToString();
+                        string imageUrl = imageToken["url"].ToString();
+
+                        Cat cat = new Cat { Id = id, Url = imageUrl };
+                        cats.Add(cat);
+                    }
+
+                    return View(cats);
+                }
+                else
+                {
+                    return View(Error);
+                }
+            }
         }
 
         public IActionResult Privacy()
---
+++ Helpers/CatHelper.cs
@@ -0,0 +1,33 @@
+﻿using CatMash.Models;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace CatMash.Helpers
+{
+    public static class CatHelper
+    {
+        public static List<Cat> GetCatsFromJson()
+        {
+            string url = "https://latelier.co/data/cats.json";
+            using (var webClient = new WebClient())
+            {
+                List<Cat> cats = new List<Cat>();
+                string json = webClient.DownloadString(url);
+
+                    JObject jsonObject = JObject.Parse(json);
+                    JArray catsArray = jsonObject["cats"] as JArray;
+
+                    foreach (JObject item in catsArray) 
+                    {
+                    string id = item.GetValue("id").ToString();
+                    string imageUrl = item.GetValue("url").ToString();
+
+                    Cat cat = new Cat { Id = id, Url = imageUrl };
+                    cats.Add(cat);
+                }
+                    return cats;
+       
+            }
+        }
+    }
+}
